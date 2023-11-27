06b6dccc3fabb89437ad422e82fc755a301b1d44
KaterynaIlkiv <61518233+KaterynaIlkiv@users.noreply.github.com>
2023-11-27 15:31:41 +0200

    Update function_return.py

--- function_return.py
+++ function_return.py
@@ -2,3 +2,9 @@ def money(money_get,money_exist,money_spend):
    return money_get + money_exist - money_spend
 
 print(money(10, 10, 5))
+
+def test_variable():
+   first_var=10
+   second_var=20
+   return first_var * second_var
+print(test_variable())
