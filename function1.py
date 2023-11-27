b097647e2978cb4531bf1a9980bcb65cdb1bca3d
KaterynaIlkiv <ilkiv.kateryna@gmail.com>
2023-11-27 15:26:44 +0200

    Added new file

---
+++ function1.py
@@ -0,0 +1,4 @@
+def say_hello(name,surname):
+   print('Hello, %s %s' %(name,surname))
+   
+say_hello('Mary','Smith')
---
+++ function_return.py
@@ -0,0 +1,4 @@
+def money(money_get,money_exist,money_spend):
+   return money_get + money_exist - money_spend
+
+print(money(10, 10, 5))
